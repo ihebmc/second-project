bbe51273c673ebe602a68641f8a0579ced41c3ef
ihebmc <iheb.mchich@esprit.tn>
2021-10-19 12:21:29 +0100

    MAJ amin

---
+++ main.c
@@ -0,0 +1,9 @@
+include <stdioh>
+ void main(){
+    int a,b,s ;
+    printf("donner les valeurs de a et b :\n");
+    scanf("%d%d",a,b);
+    s=a+b;
+    printf("voici la somme s : %d",s);
+    }
+
